626036c0c2457065d2ebe8b3b250b19a0e0f078c
Waqas <wakassadik@gmail.com>
2021-05-12 15:42:28 +0500

    adding ingredients dynamically using shopping list edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, ViewChildren } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('inputName', {static: false}) inputNameRef: ElementRef;
+  @ViewChild('inputAmount', {static: false}) inputAmountRef: ElementRef;
+ // ingredientAdded = new EventEmitter<{name:string, amout:number}>();
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem(){
+    const ingName = this.inputNameRef.nativeElement.value;
+    const ingAmount = this.inputAmountRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
